18e9bba36d0f019c5ff1950ee3a4e69cfa39e800
Abir EL HALIMI <45515682+abirelhalimi@users.noreply.github.com>
2023-09-19 14:31:53 +0200

    add - grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,28 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { GameDetail } from "./components/GameDetail";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <GameDetail />;
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"coral"}>
+        nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"gold"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
