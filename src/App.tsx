a80783c4d90917ad659faef00ca7cb5547bfab82
Abir EL HALIMI <45515682+abirelhalimi@users.noreply.github.com>
2023-09-19 13:15:45 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { GameDetail } from "./components/GameDetail";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GameDetail />;
+    </>
+  );
+}
+
+export default App;
